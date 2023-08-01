ff866af85783204d7e70fe20110478ede43f6342
GO-Grow-Online <julien.growonline@gmail.com>
2023-08-01 17:06:53 +0200

    tweak

--- send_form.php
+++ send_form.php
@@ -1,7 +1,7 @@
 <?php
 
-// use PHPMailer\PHPMailer\PHPMailer;
-// use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 require '/assets/PHPMailer-master/src/Exception.php';
 require '/assets/PHPMailer-master/src/PHPMailer.php';
