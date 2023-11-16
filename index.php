d84dee30cda0c4d35ed56b884161c367a924d0a0
GO-Grow-Online <julien.growonline@gmail.com>
2023-11-16 17:53:12 +0100

    init 2 :-)

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The main template file
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists
+ *
+ * Methods for TimberHelper can be found in the /lib sub-directory
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ * @since   Timber 0.1
+ */
+
+$context          = Timber::context();
+$templates        = array( 'index.twig' );
+
+// Is is blog
+if ( is_home() || is_archive() && 'post' == get_post_type() ) {
+	array_unshift( $templates, 'blog.twig', 'page.twig' );
+	$context['archive'] = new Timber\PostQuery();
+	$context['p'] = new Timber\Post();
+}
+
+Timber::render( $templates, $context );
