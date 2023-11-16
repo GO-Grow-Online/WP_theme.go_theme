4d499993a0eb2bd0c6f766a99555e2bd4d7d3c47
GO-Grow-Online <julien.growonline@gmail.com>
2023-11-16 17:51:53 +0100

    tweaks

--- page-legal.php
+++
@@ -1,8 +0,0 @@
-<?php
-/* Template Name: Legal pages */
-
-$context = Timber::context();
-
-$timber_post     = new Timber\Post();
-$context['p'] = $timber_post;
-Timber::render( 'page-legal.twig' , $context );
